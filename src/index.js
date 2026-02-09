10c1a2d2e8409626837a1e9e8af62e49fc6c8c3f
Alesandro <alesandroquirosg@icloud.com>
2026-02-09 15:04:21 +0100

    Estructura base del proyecto creada

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+const PORT = 8080;
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
